ae7536743981db57aad353ca9ad40c91e8675141
Gowtham Kudupudi <gowtham@ferryfair.com>
2018-07-31 15:31:54 +0530

    implemented

---
+++ Demo1.cpp
@@ -0,0 +1,10 @@
+#include "Demo1.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Demo1_::Demo1_ () {
+   cout << "Demo1 object created." << endl;
+}
+
---
+++ Demo1.h
@@ -0,0 +1,9 @@
+#ifndef DEMO1_H
+#define DEMO1_H
+
+class Demo1_ {
+public:
+   Demo1_ ();
+};
+
+#endif
